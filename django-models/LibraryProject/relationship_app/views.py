453e66f07c41b90c12b08cab84ade760dd7f7a51
okoli240 <okoli240@gmail.com>
2025-07-20 15:54:06 +0000

    revised

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book  # Keep if needed
-from .models import Library  # ✅ This exact line for the checker
+from django.views.generic.detail import DetailView  # ✅ Must match this exactly
+from .models import Book
+from .models import Library
 
 
 # Function-based view
