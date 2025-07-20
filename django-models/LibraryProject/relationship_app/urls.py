393de6d38056d26fcf2a9158a13ccd1134d7b600
okoli240 <okoli240@gmail.com>
2025-07-20 18:49:53 +0000

    finallll

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 
 urlpatterns = [
     # Book list and details
