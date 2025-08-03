f4b83e59433e8d5a581c4c964c15b870ced5254a
okoli240 <okoli240@gmail.com>
2025-08-03 10:28:28 +0000

    task 4

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include
 from rest_framework.routers import DefaultRouter
 from .views import BookList 
 from .views import BookViewSet
-
+from rest_framework.authtoken.views import obtain_auth_token
 # Set up the DRF router
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -11,7 +11,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 
-
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <-- Token endpoint
     # All CRUD operations handled by router
     path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,3 +12,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # <-- Secure the endpoint
