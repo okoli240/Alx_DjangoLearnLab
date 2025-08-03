1bcfaec61f0c0e800cc65a5e21d47abcda71c2e1
okoli240 <okoli240@gmail.com>
2025-08-03 10:18:42 +0000

    task 3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import BookList
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from .views import BookList 
+from .views import BookViewSet
+
+# Set up the DRF router
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+
+
+    # All CRUD operations handled by router
+    path('', include(router.urls)),
 ]
+
